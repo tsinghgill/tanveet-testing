b004f4eac91fd2837a845844e81356c125b14802
Meroxa <meroxa@Meroxas-MacBook-Pro.local>
2022-11-01 14:21:47 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ exports.App = class App {
     //   );
     // });
 
+    console.log(`Streaming over data to S3`)
+
     // Use records `unwrap` transform on CDC formatted records
     // Has no effect on other formats
     // records.unwrap();
@@ -31,14 +33,14 @@ exports.App = class App {
 
     // Identify the upstream datastore with the `resources` function
     // Replace `source_name` with the resource name configured on Meroxa
-    let source = await turbine.resources("migration_users_app_postgres");
+    let source = await turbine.resources("meroxa_upsolver_pg");
 
     // Specify which `source` records to pull with the `records` function
     // Replace `collection_name` with whatever data organisation method
     // is relevant to the datastore (e.g., table, bucket, collection, etc.)
     // If additional connector configs are needed, provided another argument i.e.
     // {"incrementing.field.name": "id"}
-    let records = await source.records("User");
+    let records = await source.records("myTable");
 
     // Specify the code to execute against `records` with the `process` function
     // Replace `Anonymize` with the function. If environment variables are needed
@@ -47,13 +49,19 @@ exports.App = class App {
 
     // Identify the upstream datastore with the `resources` function
     // Replace `source_name` with the resource name configured on Meroxa
-    let destination = await turbine.resources("migration_users_app_postgres");
+    let destination = await turbine.resources("meroxa_upsolver_s3");
 
     // Specify where to write records to your `destination` using the `write` function
     // Replace `collection_archive` with whatever data organisation method
     // is relevant to the datastore (e.g., table, bucket, collection, etc.)
     // If additional connector configs are needed, provided another argument i.e.
     // {"behavior.on.null.values": "ignore"}
-    await destination.write(anonymized, "collection_archive");
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+    const writeTo = `${yyyy}/${mm}/${dd}/${today.getHours()}/${today.getMinutes()}`
+    await destination.write(anonymized, writeTo);
   }
 };
