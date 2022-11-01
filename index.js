301c0bc9939e5e1faa5714de38d2947058ba812a
Meroxa <meroxa@Meroxas-MacBook-Pro.local>
2022-11-01 16:59:43 -0400

    Test

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ exports.App = class App {
 
 
     await destination.write(anonymized, `new_destination_new`, {
-      "output_compression": "none"
+      "file.name.template": "{{timestamp:unit=yyyy}}/{{timestamp:unit=MM}}/{{timestamp:unit=DD}}/{{timestamp:unit=hh}}/{{timestamp:unit=mm}}/{{topic}}-{{partition:padding=true}}-{{start_offset:padding=true}}.gz"
     });
   }
 };
