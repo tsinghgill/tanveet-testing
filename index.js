a73f3a6510819544a59a77f6fe8de1c916ddcfdd
Meroxa <meroxa@Meroxas-MacBook-Pro.local>
2022-11-21 12:32:54 -0500

    Update

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// // Import any of your relevant dependencies
-// const stringHash = require("string-hash");
-
-// // Sample helper
-// function iAmHelping(str) {
-//   return `~~~${str}~~~`;
-// }
-
 exports.App = class App {
   anonymize(records) {
     console.log(`Streaming over data to S3 at: ${`${new Date().getHours()}/${new Date().getMinutes()}`}`)
